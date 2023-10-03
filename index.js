bee870b94d5057e64409b34266a6978ec7a97f15
Blackstars <nelson.64@live.fr>
2023-10-03 10:47:58 +0200

    atelier chalk

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+let calipso = console.log(chalk.blue("Je suis Calipso"));
+let alexendre = console.log(chalk.red("Je suis Alexendre"));
+let xavier = console.log(chalk.bold.yellowBright("Je suis Xavier"));
+let prasanna = console.log(chalk.green("Je suis Prasanna"));
